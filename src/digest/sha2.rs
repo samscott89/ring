a4b2a053ab0020d8fc533cecf7c1adfcd1a935d0
Sam Scott <sam.scott89@gmail.com>
2016-06-06 11:37:03 +0100

    More loop unrolling.

--- src/digest/sha2.rs
+++ src/digest/sha2.rs
@@ -36,22 +36,22 @@ const CHAINING_WORDS_256: usize = CHAINING_LEN_256 / 4;
 const CHAINING_WORDS_512: usize = CHAINING_LEN_512 / 8;
 
 macro_rules! ch {
-    ($x:expr, $y:expr, $z:expr) => (($x & $y) ^ (!$x & $z))
+    ($x:ident, $y:ident, $z:ident) => (($x & $y) ^ (!$x & $z))
 }
 macro_rules! parity {
-    ($x:expr, $y:expr, $z:expr) => ($x ^ $y ^ $z)
+    ($x:ident, $y:ident, $z:ident) => ($x ^ $y ^ $z)
 }
 macro_rules! maj {
-    ($x:expr, $y:expr, $z:expr) => (($x & $y) ^ ($x & $z) ^ ($y & $z))
+    ($x:ident, $y:ident, $z:ident) => (($x & $y) ^ ($x & $z) ^ ($y & $z))
 }
 
-// SHA256 functions
+// SHA-256 functions
 #[inline] fn big_s0_256(x: u32) -> u32   { ((x.rotate_right(9)  ^ x).rotate_right(11) ^ x).rotate_right(2) }
 #[inline] fn big_s1_256(x: u32) -> u32   { ((x.rotate_right(14) ^ x).rotate_right(5)  ^ x).rotate_right(6) }
 #[inline] fn small_s0_256(x: u32) -> u32 { x.rotate_right(7)  ^ x.rotate_right(18) ^ (x >> 3) }
 #[inline] fn small_s1_256(x: u32) -> u32 { x.rotate_right(17) ^ x.rotate_right(19) ^ (x >> 10) }
 
-// SHA512 functions
+// SHA-512 functions
 #[inline] fn big_s0_512(x: u64) -> u64   { ((x.rotate_right(5)  ^ x).rotate_right(6) ^ x).rotate_right(28) }
 #[inline] fn big_s1_512(x: u64) -> u64   { ((x.rotate_right(23) ^ x).rotate_right(4) ^ x).rotate_right(14) }
 #[inline] fn small_s0_512(x: u64) -> u64 { x.rotate_right(1)  ^ x.rotate_right(8)  ^ (x >> 7) }
@@ -63,6 +63,116 @@ pub unsafe extern fn sha256_block_data_order(state: &mut [u64; MAX_CHAINING_LEN 
     let data = data as *const [u8; BLOCK_LEN_256];
     let blocks = core::slice::from_raw_parts(data, num);
     sha256_block_data_order_safe(state, blocks)
+}
+
+// SHA-256 message scheduling. Four iterations in one.
+macro_rules! msched_256 {
+    ($x0:ident,$x1:ident,$x2:ident,$x3:ident,$xt:ident) => {
+      $xt[0] = small_s0_256($x0[1])
+            .wrapping_add($x0[0])
+            .wrapping_add($x2[1])
+            .wrapping_add(small_s1_256($x3[2]));
+
+      $xt[1] = small_s0_256($x0[2])
+            .wrapping_add($x0[1])
+            .wrapping_add($x2[2])
+            .wrapping_add(small_s1_256($x3[3]));
+
+      $xt[2] = small_s0_256($x0[3])
+            .wrapping_add($x0[2])
+            .wrapping_add($x2[3])
+            .wrapping_add(small_s1_256($xt[0]));
+
+      $xt[3] = small_s0_256($x1[0])
+            .wrapping_add($x0[3])
+            .wrapping_add($x3[0])
+            .wrapping_add(small_s1_256($xt[1]));
+    }
+}
+
+// SHA-512 message scheduling. Four iterations in one.
+macro_rules! msched_512 {
+    ($x0:ident,$x1:ident,$x2:ident,$x3:ident,$xt:ident) => (
+      $xt[0] = small_s1_512($x3[2])
+            .wrapping_add($x2[1])
+            .wrapping_add(small_s0_512($x0[1]))
+            .wrapping_add($x0[0]);
+
+      $xt[1] = small_s1_512($x3[3])
+            .wrapping_add($x2[2])
+            .wrapping_add(small_s0_512($x0[2]))
+            .wrapping_add($x0[1]);
+
+      $xt[2] = small_s1_512($xt[0])
+            .wrapping_add($x2[3])
+            .wrapping_add(small_s0_512($x0[3]))
+            .wrapping_add($x0[2]);
+
+      $xt[3] = small_s1_512($xt[1])
+            .wrapping_add($x3[0])
+            .wrapping_add(small_s0_512($x1[0]))
+            .wrapping_add($x0[3]);
+    )
+}
+
+// SHA-256 round functions.
+macro_rules! round_256 {
+    ($i:expr,$ti:expr,$x:ident,$a:ident,$b:ident,$c:ident,$d:ident,$e:ident,$f:ident,$g:ident,$h:ident) => {{
+          let t1 = $h.wrapping_add(big_s1_256($e))
+                    .wrapping_add(ch!($e, $f, $g))
+                    .wrapping_add(K_256[$ti])
+                    .wrapping_add($x[$i]);
+          let t2 = big_s0_256($a).wrapping_add(maj!($a,$b,$c));
+          $h = $g;
+          $g = $f;
+          $f = $e;
+          $e = $d.wrapping_add(t1);
+          $d = $c;
+          $c = $b;
+          $b = $a;
+          $a = t1.wrapping_add(t2);
+        }}
+}
+
+// SHA-256 round functions, four iterations.
+macro_rules! round_256_x4 {
+    ($t:expr,$w:ident,$a:ident,$b:ident,$c:ident,$d:ident,$e:ident,$f:ident,$g:ident,$h:ident) => {{
+          let x = $w[$t];
+          round_256!(0,4*$t+0, x, $a,$b,$c,$d,$e,$f,$g,$h);
+          round_256!(1,4*$t+1, x, $a,$b,$c,$d,$e,$f,$g,$h);
+          round_256!(2,4*$t+2, x, $a,$b,$c,$d,$e,$f,$g,$h);
+          round_256!(3,4*$t+3, x, $a,$b,$c,$d,$e,$f,$g,$h);
+        }}
+}
+
+// SHA-512 round functions.
+macro_rules! round_512 {
+    ($i:expr,$ti:expr,$x:ident,$a:ident,$b:ident,$c:ident,$d:ident,$e:ident,$f:ident,$g:ident,$h:ident) => {{
+          let t1 = $h.wrapping_add(big_s1_512($e))
+                    .wrapping_add(ch!($e, $f, $g))
+                    .wrapping_add(K_512[$ti])
+                    .wrapping_add($x[$i]);
+          let t2 = big_s0_512($a).wrapping_add(maj!($a,$b,$c));
+          $h = $g;
+          $g = $f;
+          $f = $e;
+          $e = $d.wrapping_add(t1);
+          $d = $c;
+          $c = $b;
+          $b = $a;
+          $a = t1.wrapping_add(t2);
+        }}
+}
+
+// SHA-512 round functions, four iterations.
+macro_rules! round_512_x4 {
+    ($t:expr,$w:ident,$a:ident,$b:ident,$c:ident,$d:ident,$e:ident,$f:ident,$g:ident,$h:ident) => {{
+          let x = $w[$t];
+          round_512!(0,4*$t+0, x, $a,$b,$c,$d,$e,$f,$g,$h);
+          round_512!(1,4*$t+1, x, $a,$b,$c,$d,$e,$f,$g,$h);
+          round_512!(2,4*$t+2, x, $a,$b,$c,$d,$e,$f,$g,$h);
+          round_512!(3,4*$t+3, x, $a,$b,$c,$d,$e,$f,$g,$h);
+        }}
 }
 
 fn sha256_block_data_order_safe(state: &mut [u64; MAX_CHAINING_LEN / 8],
@@ -84,39 +194,46 @@ fn sha256_block_data_order_safe(state: &mut [u64; MAX_CHAINING_LEN / 8],
             }
         }
 
-        let mut x0 = w[0];
-        let mut x1 = w[1];
-        let mut x2 = w[2];
-        let mut x3 = w[3];
-
-        for t in 4..16 {
-            let mut xt = w[t];
-            xt[0] = small_s0_256(x0[1])
-                  .wrapping_add(x0[0])
-                  .wrapping_add(x2[1])
-                  .wrapping_add(small_s1_256(x3[2]));
-
-            xt[1] = small_s0_256(x0[2])
-                  .wrapping_add(x0[1])
-                  .wrapping_add(x2[2])
-                  .wrapping_add(small_s1_256(x3[3]));
-            
-            xt[2] = small_s0_256(x0[3])
-                  .wrapping_add(x0[2])
-                  .wrapping_add(x2[3])
-                  .wrapping_add(small_s1_256(xt[0]));
-            
-            xt[3] = small_s0_256(x1[0])
-                  .wrapping_add(x0[3])
-                  .wrapping_add(x3[0])
-                  .wrapping_add(small_s1_256(xt[1]));
-
-            x0 = x1;
-            x1 = x2;
-            x2 = x3;
-            x3 = xt;
-            w[t] = xt;
-        }
+        let mut x0 = w[0]; // w[t-16], w[t-15], w[t-14], w[t-13]
+        let mut x1 = w[1]; // w[t-12], w[t-11], w[t-10], w[t-9]
+        let mut x2 = w[2]; // w[t-8],  w[t-7],  w[t-6],  w[t-5]
+        let mut x3 = w[3]; // w[t-4],  w[t-3],  w[t-2],  w[t-1]
+        let mut xt = w[4];
+        msched_256!(x0, x1, x2, x3, xt);
+        w[4] = xt;
+        x0 = w[5];
+        msched_256!(x1, x2, x3, xt, x0);
+        w[5] = x0;
+        x1 = w[6];
+        msched_256!(x2, x3, xt, x0, x1);
+        w[6] = x1;
+        x2 = w[7];
+        msched_256!(x3, xt, x0, x1, x2);
+        w[7] = x2;
+        x3 = w[8];
+        msched_256!(xt, x0, x1, x2, x3);
+        w[8] = x3;
+        xt = w[9];
+        msched_256!(x0, x1, x2, x3, xt);
+        w[9] = xt;
+        x0 = w[10];
+        msched_256!(x1, x2, x3, xt, x0);
+        w[10] = x0;
+        x1 = w[11];
+        msched_256!(x2, x3, xt, x0, x1);
+        w[11] = x1;
+        x2 = w[12];
+        msched_256!(x3, xt, x0, x1, x2);
+        w[12] = x2;
+        x3 = w[13];
+        msched_256!(xt, x0, x1, x2, x3);
+        w[13] = x3;
+        xt = w[14];
+        msched_256!(x0, x1, x2, x3, xt);
+        w[14] = xt;
+        x0 = w[15];
+        msched_256!(x1, x2, x3, xt, x0);
+        w[15] = x0;
 
         let mut a = state[0];
         let mut b = state[1];
@@ -127,64 +244,23 @@ fn sha256_block_data_order_safe(state: &mut [u64; MAX_CHAINING_LEN / 8],
         let mut g = state[6];
         let mut h = state[7];
 
-        for t in 0..16 {
-            let x = w[t];
-            let t1 = h.wrapping_add(big_s1_256(e))
-                      .wrapping_add(ch!(e, f, g))
-                      .wrapping_add(K_256[4*t])
-                      .wrapping_add(x[0]);
-            let t2 = big_s0_256(a).wrapping_add(maj!(a,b,c));
-            h = g; 
-            g = f;
-            f = e;
-            e = d.wrapping_add(t1);
-            d = c;
-            c = b; 
-            b = a;
-            a = t1.wrapping_add(t2);
+        round_256_x4!(0, w, a,b,c,d,e,f,g,h);
+        round_256_x4!(1, w, a,b,c,d,e,f,g,h);
+        round_256_x4!(2, w, a,b,c,d,e,f,g,h);
+        round_256_x4!(3, w, a,b,c,d,e,f,g,h);
+        round_256_x4!(4, w, a,b,c,d,e,f,g,h);
+        round_256_x4!(5, w, a,b,c,d,e,f,g,h);
+        round_256_x4!(6, w, a,b,c,d,e,f,g,h);
+        round_256_x4!(7, w, a,b,c,d,e,f,g,h);
+        round_256_x4!(8, w, a,b,c,d,e,f,g,h);
+        round_256_x4!(9, w, a,b,c,d,e,f,g,h);
+        round_256_x4!(10, w, a,b,c,d,e,f,g,h);
+        round_256_x4!(11, w, a,b,c,d,e,f,g,h);
+        round_256_x4!(12, w, a,b,c,d,e,f,g,h);
+        round_256_x4!(13, w, a,b,c,d,e,f,g,h);
+        round_256_x4!(14, w, a,b,c,d,e,f,g,h);
+        round_256_x4!(15, w, a,b,c,d,e,f,g,h);
 
-            let t1 = h.wrapping_add(big_s1_256(e))
-                      .wrapping_add(ch!(e, f, g))
-                      .wrapping_add(K_256[4*t+1])
-                      .wrapping_add(x[1]);
-            let t2 = big_s0_256(a).wrapping_add(maj!(a,b,c));
-            h = g;
-            g = f;
-            f = e;
-            e = d.wrapping_add(t1);
-            d = c;
-            c = b;
-            b = a;
-            a = t1.wrapping_add(t2);
-
-            let t1 = h.wrapping_add(big_s1_256(e))
-                      .wrapping_add(ch!(e, f, g))
-                      .wrapping_add(K_256[4*t+2])
-                      .wrapping_add(x[2]);
-            let t2 = big_s0_256(a).wrapping_add(maj!(a,b,c));
-            h = g;
-            g = f;
-            f = e;
-            e = d.wrapping_add(t1);
-            d = c;
-            c = b;
-            b = a;
-            a = t1.wrapping_add(t2);
-
-            let t1 = h.wrapping_add(big_s1_256(e))
-                      .wrapping_add(ch!(e, f, g))
-                      .wrapping_add(K_256[4*t+3])
-                      .wrapping_add(x[3]);
-            let t2 = big_s0_256(a).wrapping_add(maj!(a,b,c));
-            h = g;
-            g = f;
-            f = e;
-            e = d.wrapping_add(t1);
-            d = c;
-            c = b;
-            b = a;
-            a = t1.wrapping_add(t2);
-        }
         state[0] = a.wrapping_add(state[0]);
         state[1] = b.wrapping_add(state[1]);
         state[2] = c.wrapping_add(state[2]);
@@ -212,18 +288,69 @@ fn sha512_block_data_order_safe(state: &mut [u64; MAX_CHAINING_LEN / 8],
     let state = slice_as_array_ref_mut!(state, CHAINING_WORDS_512).unwrap();
 
     // Message schedule Wt
-    let mut w: [u64; 80] = [0; 80];
+    let mut w: [[u64; 4]; 20] = [[0; 4]; 20];
+
     for block in blocks {
-        for t in 0..16 {
-            let word = slice_as_array_ref!(&block[t * 8..][..8], 8).unwrap();
-            w[t] = polyfill::slice::u64_from_be_u8(word)
+        for t in 0..4 {
+            for i in 0..4 {
+                let word = slice_as_array_ref!(&block[(4*t + i) * 8..][..8], 8).unwrap();
+                w[t][i] = polyfill::slice::u64_from_be_u8(word);
+            }
         }
-        for t in 16..80 {
-            w[t] = small_s1_512(w[t - 2])
-                   .wrapping_add(w[t - 7])
-                   .wrapping_add(small_s0_512(w[t - 15]))
-                   .wrapping_add(w[t - 16]);
-        }
+
+        let mut x0 = w[0]; // w[t-16], w[t-15], w[t-14], w[t-13]
+        let mut x1 = w[1]; // w[t-12], w[t-11], w[t-10], w[t-9]
+        let mut x2 = w[2]; // w[t-8],  w[t-7],  w[t-6],  w[t-5]
+        let mut x3 = w[3]; // w[t-4],  w[t-3],  w[t-2],  w[t-1]
+        let mut xt = w[4];
+        msched_512!(x0, x1, x2, x3, xt);
+        w[4] = xt;
+        x0 = w[5];
+        msched_512!(x1, x2, x3, xt, x0);
+        w[5] = x0;
+        x1 = w[6];
+        msched_512!(x2, x3, xt, x0, x1);
+        w[6] = x1;
+        x2 = w[7];
+        msched_512!(x3, xt, x0, x1, x2);
+        w[7] = x2;
+        x3 = w[8];
+        msched_512!(xt, x0, x1, x2, x3);
+        w[8] = x3;
+        xt = w[9];
+        msched_512!(x0, x1, x2, x3, xt);
+        w[9] = xt;
+        x0 = w[10];
+        msched_512!(x1, x2, x3, xt, x0);
+        w[10] = x0;
+        x1 = w[11];
+        msched_512!(x2, x3, xt, x0, x1);
+        w[11] = x1;
+        x2 = w[12];
+        msched_512!(x3, xt, x0, x1, x2);
+        w[12] = x2;
+        x3 = w[13];
+        msched_512!(xt, x0, x1, x2, x3);
+        w[13] = x3;
+        xt = w[14];
+        msched_512!(x0, x1, x2, x3, xt);
+        w[14] = xt;
+        x0 = w[15];
+        msched_512!(x1, x2, x3, xt, x0);
+        w[15] = x0;
+        x1 = w[16];
+        msched_512!(x2, x3, xt, x0, x1);
+        w[16] = x1;
+        x2 = w[17];
+        msched_512!(x3, xt, x0, x1, x2);
+        w[17] = x2;
+        x3 = w[18];
+        msched_512!(xt, x0, x1, x2, x3);
+        w[18] = x3;
+        xt = w[19];
+        msched_512!(x0, x1, x2, x3, xt);
+        w[19] = xt;
+
         let mut a = state[0];
         let mut b = state[1];
         let mut c = state[2];
@@ -233,21 +360,27 @@ fn sha512_block_data_order_safe(state: &mut [u64; MAX_CHAINING_LEN / 8],
         let mut g = state[6];
         let mut h = state[7];
 
-        for t in 0..80 {
-            let t1 = h.wrapping_add(big_s1_512(e))
-                      .wrapping_add(ch!(e, f, g))
-                      .wrapping_add(K_512[t])
-                      .wrapping_add(w[t]);
-            let t2 = big_s0_512(a).wrapping_add(maj!(a,b,c));
-            h = g;
-            g = f;
-            f = e;
-            e = d.wrapping_add(t1);
-            d = c;
-            c = b;
-            b = a;
-            a = t1.wrapping_add(t2);
-        }
+        round_512_x4!(0, w, a,b,c,d,e,f,g,h);
+        round_512_x4!(1, w, a,b,c,d,e,f,g,h);
+        round_512_x4!(2, w, a,b,c,d,e,f,g,h);
+        round_512_x4!(3, w, a,b,c,d,e,f,g,h);
+        round_512_x4!(4, w, a,b,c,d,e,f,g,h);
+        round_512_x4!(5, w, a,b,c,d,e,f,g,h);
+        round_512_x4!(6, w, a,b,c,d,e,f,g,h);
+        round_512_x4!(7, w, a,b,c,d,e,f,g,h);
+        round_512_x4!(8, w, a,b,c,d,e,f,g,h);
+        round_512_x4!(9, w, a,b,c,d,e,f,g,h);
+        round_512_x4!(10, w, a,b,c,d,e,f,g,h);
+        round_512_x4!(11, w, a,b,c,d,e,f,g,h);
+        round_512_x4!(12, w, a,b,c,d,e,f,g,h);
+        round_512_x4!(13, w, a,b,c,d,e,f,g,h);
+        round_512_x4!(14, w, a,b,c,d,e,f,g,h);
+        round_512_x4!(15, w, a,b,c,d,e,f,g,h);
+        round_512_x4!(16, w, a,b,c,d,e,f,g,h);
+        round_512_x4!(17, w, a,b,c,d,e,f,g,h);
+        round_512_x4!(18, w, a,b,c,d,e,f,g,h);
+        round_512_x4!(19, w, a,b,c,d,e,f,g,h);
+
         state[0] = a.wrapping_add(state[0]);
         state[1] = b.wrapping_add(state[1]);
         state[2] = c.wrapping_add(state[2]);
@@ -259,7 +392,7 @@ fn sha512_block_data_order_safe(state: &mut [u64; MAX_CHAINING_LEN / 8],
     }
 }
 
-// SHA256 constants K
+// SHA-256 constants K
 const K_256: [u32; 64] = [0x428a2f98, 0x71374491, 0xb5c0fbcf, 0xe9b5dba5,
     0x3956c25b, 0x59f111f1, 0x923f82a4, 0xab1c5ed5, 0xd807aa98, 0x12835b01,
     0x243185be, 0x550c7dc3, 0x72be5d74, 0x80deb1fe, 0x9bdc06a7, 0xc19bf174,
@@ -272,7 +405,7 @@ const K_256: [u32; 64] = [0x428a2f98, 0x71374491, 0xb5c0fbcf, 0xe9b5dba5,
     0x391c0cb3, 0x4ed8aa4a, 0x5b9cca4f, 0x682e6ff3, 0x748f82ee, 0x78a5636f,
     0x84c87814, 0x8cc70208, 0x90befffa, 0xa4506ceb, 0xbef9a3f7, 0xc67178f2];
 
-// SHA512 constants K
+// SHA-512 constants K
 const K_512: [u64; 80] = [0x428a2f98d728ae22, 0x7137449123ef65cd,
       0xb5c0fbcfec4d3b2f, 0xe9b5dba58189dbbc, 0x3956c25bf348b538,
       0x59f111f1b605d019, 0x923f82a4af194f9b, 0xab1c5ed5da6d8118,
